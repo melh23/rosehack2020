d57f5ab1787c9efcd778a5d12786c05fe203bab0
melh23 <melhenn23@gmail.com>
2020-01-22 19:49:45 -0800

    Added assets folder

---
+++ Assets/disappear.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disappear : MonoBehaviour
+{
+    public GameObject heart;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+        {
+           heart.transform.Rotate(Vector3.right * Time.deltaTime * 5);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        heart.SetActive(false);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/rotateScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateScript : MonoBehaviour
+{
+
+    public GameObject maze;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        maze.transform.Rotate(0, 0, -180, Space.Self);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //maze.transform.Rotate(Vector3.right * Time.deltaTime*20);
+        // maze.transform.Rotate(Vector3.right * 180);
+        //maze.transform.Rotate(0, 0, 180, Space.Self);
+    }
+}
